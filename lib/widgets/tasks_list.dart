504723f838c6543a62fbbed1b75110b7d6fb6360
Rehan Khan <rehankhancr25@gmail.com>
2019-08-24 19:28:32 +0530

    Initial Commit

---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'package:todoey_flutter/widgets/taks_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
+          return TaskTile(
+            taskTitle: task.name,
+            isChecked: task.isDone,
+            checkboxCallback: (bool checkboxState) {
+              taskData.updateTask(task);
+            },
+            longPressCallback: () {
+              taskData.deleteTask(task);
+            },
+          );
+        },
+        itemCount: taskData.taskCount,
+      );
+    });
+  }
+}
